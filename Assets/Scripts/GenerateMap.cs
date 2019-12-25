583be35e1db5ec4c4dc331d3a020cce69d5d6a70
Ismail Koksal <ismail@koksal.fr>
2019-12-25 23:41:37 +0100

    first commit

---
+++ Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour
+{
+    [SerializeField] GameObject mapParent;
+    [SerializeField] GameObject tilePrefab;
+    [SerializeField] GameObject playerPrefab;
+
+    private GameObject[,] tileList = new GameObject[10, 10];
+
+    void Start()
+    {
+        InstantiateTiles();
+    }
+
+    void InstantiateTiles()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int z = 0; z < 10; z++)
+            {
+                tileList[x, z] = Instantiate(tilePrefab, new Vector3(x, 0f, z), new Quaternion(), mapParent.transform);
+            }
+        }
+        Instantiate(playerPrefab, new Vector3(0f, 1.3f, 5f), new Quaternion());
+    }
+}
---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public void LoadScene(Object scene)
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+}
